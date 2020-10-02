4fce710f615aa42c9eef8b71bc53a18265d63440
Kavya <55911680+KavyaRayidi@users.noreply.github.com>
2020-10-03 03:51:41 +0530

    Update Matrix multiplication.c
    added comments and a header file

--- Matrix multiplication.c
+++ Matrix multiplication.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
- 
+#include<stdio.h>
+#include<math.h>
+
 int main()
 {
+//declare the variables
   int m, n, p, q, c, d, k, sum = 0;
   int first[10][10], second[10][10], multiply[10][10];
- 
+
+ //enter the number of rows, columns and elements in first matrix
   printf("Enter the number of rows and columns of first matrix\n");
   scanf("%d%d", &m, &n);
   printf("Enter the elements of first matrix\n");
@@ -12,10 +15,12 @@ int main()
   for (  c = 0 ; c < m ; c++ )
     for ( d = 0 ; d < n ; d++ )
       scanf("%d", &first[c][d]);
- 
+
+ //enter the number of rows, columns and elements in second matrix
   printf("Enter the number of rows and columns of second matrix\n");
   scanf("%d%d", &p, &q);
  
+ //conditions
   if ( n != p )
     printf("Matrices with entered orders can't be multiplied with each other.\n");
   else
@@ -40,6 +45,7 @@ int main()
       }
     }
  
+   //print the product matrix
     printf("Product of entered matrices:-\n");
  
     for ( c = 0 ; c < m ; c++ )
