9dcc1826e1efaaa7b9b6a0f9b96d16c4e47fb109
Kavya <55911680+KavyaRayidi@users.noreply.github.com>
2020-10-03 03:54:40 +0530

    Update normal and trace.c
    added comments

--- normal and trace.c
+++ normal and trace.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <math.h>
  
-    void main ()
+void main ()
     {
  
+ //declare the variables
         static int array[10][10];
         int i, j, m, n, sum = 0, sum1 = 0, a = 0, normal;
  
+ //input order
         printf("Enter the order of the matrix\n");
         scanf("%d %d", &m, &n);
- 
+
+ //input coefficients
         printf("Enter the n coefficients of the matrix \n");
+ 
+ //perform operations
         for (i = 0; i < m; ++i) 
         {
             for (j = 0; j < n; ++j)
@@ -21,6 +26,7 @@
             }
         }
  
+ //print the normal
         normal = sqrt(sum1);
         printf("The normal of the given matrix is = %d\n", normal);
         for (i = 0; i < m; ++i) 
@@ -28,6 +34,7 @@
             sum = sum + array[i][i];
         }
  
+ //print the trace
         printf("Trace of the matrix is = %d\n", sum);
  
     }
