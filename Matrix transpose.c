800e544a7ab7f363f1de73be5a34c7002dd20486
KavyaRayidi <55911680+KavyaRayidi@users.noreply.github.com>
2020-10-03 03:40:01 +0530

    Update Matrix transpose.c
    displayed the matrix and improved the comments

--- Matrix transpose.c
+++ Matrix transpose.c
@@ -1,28 +1,47 @@
-#include <stdio.h>
-int main() {
+#include<stdio.h>
+#include<math.h>
+
+int main() 
+{
     int a[10][10], transpose[10][10], r, c, i, j;
     printf("Enter rows and columns: ");
     scanf("%d %d", &r, &c);
 
-    // Assigning elements to the matrix
+    // Assigning elements to different positions in the matrix
     printf("\nEnter matrix elements:\n");
     for (i = 0; i < r; ++i)
-        for (j = 0; j < c; ++j) {
+        for (j = 0; j < c; ++j) 
+            
+        {
             printf("Enter element a%d%d: ", i + 1, j + 1);
             scanf("%d", &a[i][j]);
         }
 
-
-    // Finding the transpose of matrix a
+    // Displaying the matrix input by user
+    printf("\nEntered matrix: \n");
     for (i = 0; i < r; ++i)
-        for (j = 0; j < c; ++j) {
+        for (j = 0; j < c; ++j) 
+        
+        {
+            printf("%d  ", a[i][j]);
+            if (j == c - 1)
+                printf("\n");
+        }
+
+    // Finding the transpose of the matrix
+    for (i = 0; i < r; ++i)
+        for (j = 0; j < c; ++j) 
+        
+        {
             transpose[j][i] = a[i][j];
         }
 
-    // Displaying the transpose of matrix a
+    // Displaying the transpose of the matrix
     printf("\nTranspose of the matrix:\n");
     for (i = 0; i < c; ++i)
-        for (j = 0; j < r; ++j) {
+        for (j = 0; j < r; ++j) 
+        
+        {
             printf("%d  ", transpose[i][j]);
             if (j == r - 1)
                 printf("\n");
